c5c0c48e3c34277a69b277c16667831d2374574e
slwu89 <slwu89@berkeley.edu>
2017-06-24 14:23:09 -0600

    Initial Commit

---
+++ cgalTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cgalTest
+//
+//  Created by Sean Wu on 6/24/17.
+//  Copyright © 2017 Sean Wu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
