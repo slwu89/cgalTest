3fe745131e232e9007a6cf76033dab13229c4abb
slwu89 <slwu89@berkeley.edu>
2017-07-01 16:17:31 -0600

    how to extract Segment_2

--- cgalTest/main.cpp
+++ cgalTest/main.cpp
@@ -30,7 +30,18 @@
 
 #include <CGAL/Voronoi_diagram_2.h>
 
+
+
+#include <CGAL/Origin.h>
+#include <CGAL/Point_2.h>
+#include <CGAL/use.h>
+//#include <CGAL/Weighted_point_2.h>
+
+//try to weight points
+
+
 // get data out
+
 
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel_Exact;
@@ -44,6 +55,7 @@ typedef CGAL::Apollonius_graph_2<AGT2_K>                                        
 typedef CGAL::Apollonius_graph_adaptation_traits_2<AG2>                            AG2_Trait;
 typedef CGAL::Apollonius_graph_caching_degeneracy_removal_policy_2<AG2>            AG2_Policy;
 typedef CGAL::Voronoi_diagram_2<AG2,AG2_Trait,AG2_Policy>                          VD_AG2;
+
 
 // typedefs to get data out
 typedef VD_AG2::Locate_result             Locate_result;
@@ -72,17 +84,17 @@ void WeightedVoronoi(){
             List_Nodes.push_back(Site_2_Apo(Site_2_Point_2(i+randomPoint(RNG),j+randomPoint(RNG)),Site_2_Weight(randomWeight(RNG))));
         }
     }
-    
+
     VD_AG2 VDA;      //Voronoi Apol
-    
+
     std::cout << "got here 2 " << std::endl;
     ///Voronoi Generation
     VDA.clear();
     VDA.insert(List_Nodes.begin(),List_Nodes.end());
     double xx = VDA.number_of_faces();
-    
+
     std::cout << "made VDA object with faces: " << xx << std::endl;
-    
+
 //    VDA.bounded_faces_iterator f;
     VD_AG2::Bounded_faces_iterator f;
 //    std::vector<CGAL::QPointF> poly;
@@ -97,8 +109,6 @@ void WeightedVoronoi(){
             std::cout << "x: " << x << " y: " << y << std::endl;
         } while ( ++ec != ec_start );
     }
-    
-    
 
 }
 
@@ -106,117 +116,68 @@ void WeightedVoronoi(){
 
 
 
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Delaunay_triangulation_2.h>
+#include <iterator>
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef K::FT Weight;
+typedef K::Point_2 Point_2;
+typedef K::Iso_rectangle_2 Iso_rectangle_2;
+typedef K::Segment_2 Segment_2;
+typedef K::Ray_2 Ray_2;
+typedef K::Line_2 Line_2;
+typedef CGAL::Delaunay_triangulation_2<K> Delaunay_triangulation_2;
 
+//A class to recover Voronoi diagram from stream.
+//Rays, lines and segments are cropped to a rectangle
+//so that only segments are stored
+struct Cropped_voronoi_from_delaunay{
+    std::list<Segment_2> m_cropped_vd;
+    Iso_rectangle_2 m_bbox;
+    Cropped_voronoi_from_delaunay(const Iso_rectangle_2& bbox):m_bbox(bbox){}
+    template <class RSL>
+    void crop_and_extract_segment(const RSL& rsl){
+        CGAL::Object obj = CGAL::intersection(rsl,m_bbox);
+        const Segment_2* s=CGAL::object_cast<Segment_2>(&obj);
+        if (s) m_cropped_vd.push_back(*s);
+    }
+    void operator<<(const Ray_2& ray) { crop_and_extract_segment(ray); }
+    void operator<<(const Line_2& line) { crop_and_extract_segment(line); }
+    void operator<<(const Segment_2& seg){ crop_and_extract_segment(seg); }
+};
 
-// pointsAndSegment
-//typedef CGAL::Simple_cartesian<double> Kernel;
-//typedef Kernel::Point_2 Point_2;
-//typedef Kernel::Segment_2 Segment_2;
-// kernel
-//typedef CGAL::Exact_predicates_exact_constructions_kernel KernelExact;
-//typedef KernelExact::Point_2 Point_2_Exact;
-////convexHullVector
-//typedef CGAL::Exact_predicates_inexact_constructions_kernel KernelInexact;
-//typedef KernelInexact::Point_2 Point_2_Inexact;
-//typedef std::vector<Point_2_Inexact> PointsInexact;
+void boundedTriangulation(){
+    //consider some points
+    std::vector<Point_2> points;
+    points.push_back(Point_2(0,0));
+    points.push_back(Point_2(1,1));
+    points.push_back(Point_2(0,1));
+    Delaunay_triangulation_2 dt2;
+    //insert points into the triangulation
+    dt2.insert(points.begin(),points.end());
+    //construct a rectangle
+    Iso_rectangle_2 bbox(-1,-1,2,2);
+    Cropped_voronoi_from_delaunay vor(bbox);
+    //extract the cropped Voronoi diagram
+    dt2.draw_dual(vor);
+    //print the cropped Voronoi diagram as segments
+    int size = vor.m_cropped_vd.size();
+    std::vector<Segment_2> out(size);
+    std::copy(vor.m_cropped_vd.begin(),vor.m_cropped_vd.end(),
+              out.begin());
 
+}
 
-//// pointsAndSegment
-//
-//void pointsAndSegment()
-//{
-//    Point_2 p(1,1), q(10,10);
-//    std::cout << "p = " << p << std::endl;
-//    std::cout << "q = " << q.x() << " " << q.y() << std::endl;
-//    std::cout << "sqdist(p,q) = "
-//    << CGAL::squared_distance(p,q) << std::endl;
-//    
-//    Segment_2 s(p,q);
-//    Point_2 m(5, 9);
-//    
-//    std::cout << "m = " << m << std::endl;
-//    std::cout << "sqdist(Segment_2(p,q), m) = "
-//    << CGAL::squared_distance(s,m) << std::endl;
-//    std::cout << "p, q, and m ";
-//    switch (CGAL::orientation(p,q,m)){
-//        case CGAL::COLLINEAR:
-//            std::cout << "are collinear\n";
-//            break;
-//        case CGAL::LEFT_TURN:
-//            std::cout << "make a left turn\n";
-//            break;
-//        case CGAL::RIGHT_TURN:
-//            std::cout << "make a right turn\n";
-//            break;
-//    }
-//    std::cout << " midpoint(p,q) = " << CGAL::midpoint(p,q) << std::endl;
-//}
-//
-//
-//// kernel
-//typedef CGAL::Exact_predicates_exact_constructions_kernel KernelExact;
-//typedef KernelExact::Point_2 Point_2_Exact;
-//
-//void kernel()
-//{
-//    Point_2_Exact p(double(0.0), 0.3), q, r(double(2.0), 0.9);
-//    {
-//        q  = Point_2_Exact(double(1.0), 0.6);
-//        std::cout << (CGAL::collinear(p,q,r) ? "collinear\n" : "not collinear\n");
-//    }
-//    
-//    {
-//        std::istringstream input("0 0.3   1 0.6   2 0.9");
-//        input >> p >> q >> r;
-//        std::cout << (CGAL::collinear(p,q,r) ? "collinear\n" : "not collinear\n");
-//    }
-//    
-//    {
-//        q = CGAL::midpoint(p,r);
-//        std::cout << (CGAL::collinear(p,q,r) ? "collinear\n" : "not collinear\n");
-//    }
-//    
-//}
-
-
-//// we can also fill the "points" from a STL vector as shown below:
-//// fill from a vector:
-//typedef CGAL::Exact_predicates_inexact_constructions_kernel KernelInexact;
-//typedef KernelInexact::Point_2 Point_2_Inexact;
-//typedef std::vector<Point_2_Inexact> PointsInexact;
-//
-//void convexHullVector(){
-//
-//    PointsInexact points, result;
-//    points.push_back(Point_2_Inexact(0,0));
-//    points.push_back(Point_2_Inexact(10,0));
-//    points.push_back(Point_2_Inexact(10,10));
-//    points.push_back(Point_2_Inexact(6,5));
-//    CGAL::convex_hull_2(points.begin(), points.end(), std::back_inserter(result));
-//    std::cout << result.size() << " points on the convex hull" << std::endl;
-//    
-//}
 
 
 int main(){
-//    std::cout << "RUNNING POINTS AND SEGMENT! " << std::endl;
-//    std::cout << "" << std::endl;
-//    pointsAndSegment();
-//    std::cout << "" << std::endl;
-//    
-//    std::cout << "RUNNING EXACT KERNEL! " << std::endl;
-//    std::cout << "" << std::endl;
-//    kernel();
-//    std::cout << "" << std::endl;
-    
-//    std::cout << "RUNNING CONVEX HULL VECTOR! " << std::endl;
-//    std::cout << "" << std::endl;
-//    convexHullVector();
-//    std::cout << "" << std::endl;
-    
+
     std::cout << "RUNNING WEIGHTED VORONOI" << std::endl;
     WeightedVoronoi();
     
+    std::cout << "RUNNING BOUNDED VORONOI" << std::endl;
+    boundedTriangulation();
+
     return(0);
-    
+
 }
